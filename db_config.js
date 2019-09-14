eeb9a8af109a680704f357eda834d73a67b90466
Tanay Amonkar <tanay.a@TRADESTONE.local>
2019-09-14 18:22:22 +0530

    faaxxssxssssss

--- db_config.js
+++ db_config.js
@@ -2,9 +2,9 @@
 var mysql = require('mysql');
 
 var db_settings = {
-	host: 'localhost',
-	user: 'root',
-	password: '',
+	host: 'us-cdbr-iron-east-02.cleardb.net',
+	user: 'b925987b32e5af',
+	password: '44f7fa8d',
 	database: 'stock_db'
 };
 var qb = require('node-querybuilder').QueryBuilder(db_settings, 'mysql', 'single');
@@ -12,5 +12,5 @@ var qb = require('node-querybuilder').QueryBuilder(db_settings, 'mysql', 'single
 module.exports.qb = qb;
 
 /*Base Url*/
-var base_url = 'http://localhost:5000/';
+var base_url = 'mysql://b925987b32e5af:44f7fa8d@us-cdbr-iron-east-02.cleardb.net/heroku_6c45bd4f8ce0063?reconnect=true';
 module.exports.base_url = base_url;
